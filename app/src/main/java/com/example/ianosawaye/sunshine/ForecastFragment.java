433795fb7bc0f30356a94be0c26434aebe2393e7
Ian osawaye <Osasosawaye44@yahoo.com>
2014-09-29 17:19:59 -0400

    Added refresh button and options menu

--- app/src/main/java/com/example/ianosawaye/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/ianosawaye/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -30,8 +33,17 @@ public class ForecastFragment extends Fragment
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
 
         ArrayList<String> weekForecast = new ArrayList<String>();
         for (int i = 0; i < 8; i++) {
@@ -46,6 +58,26 @@ public class ForecastFragment extends Fragment
 
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        int id = item.getItemId();
+        if(id == R.id.action_refresh )
+        {
+            new FetchWeatherTask().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
